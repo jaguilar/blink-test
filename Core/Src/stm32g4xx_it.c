072c4aa1f6750c42cce8b575824900675426b6d9
James Aguilar <aguilar.james@gmail.com>
2026-04-06 14:16:27 -0600

    Add more extensive handler faults printing.

--- Core/Src/stm32g4xx_it.c
+++ Core/Src/stm32g4xx_it.c
@@ -61,7 +61,76 @@
 extern TIM_HandleTypeDef htim17;
 
 /* USER CODE BEGIN EV */
+__attribute__((used)) void UsageFault_Handler_C(uint32_t *stacked_regs)
+{
+    uint32_t r0  = stacked_regs[0];
+    uint32_t r1  = stacked_regs[1];
+    uint32_t r2  = stacked_regs[2];
+    uint32_t r3  = stacked_regs[3];
+    uint32_t r12 = stacked_regs[4];
+    uint32_t lr  = stacked_regs[5];
+    uint32_t pc  = stacked_regs[6];
+    uint32_t xpsr = stacked_regs[7];
 
+    printf("\r\n--- UsageFault Handler ---\r\n");
+    printf("CFSR: 0x%08x\r\n", (unsigned int)SCB->CFSR);
+    printf("Stacked R0:  0x%08x\r\n", (unsigned int)r0);
+    printf("Stacked R1:  0x%08x\r\n", (unsigned int)r1);
+    printf("Stacked R2:  0x%08x\r\n", (unsigned int)r2);
+    printf("Stacked R3:  0x%08x\r\n", (unsigned int)r3);
+    printf("Stacked R12: 0x%08x\r\n", (unsigned int)r12);
+    printf("Stacked LR:  0x%08x\r\n", (unsigned int)lr);
+    printf("Stacked PC:  0x%08x\r\n", (unsigned int)pc);
+    printf("Stacked PSR: 0x%08x\r\n", (unsigned int)xpsr);
+
+    if (SCB->CFSR & (1 << 17)) {
+        printf("Cause: INVSTATE (Invalid State)\r\n");
+    }
+    if (SCB->CFSR & (1 << 16)) {
+        printf("Cause: UNDEFINSTR (Undefined Instruction)\r\n");
+    }
+    if (SCB->CFSR & (1 << 18)) {
+        printf("Cause: INVPC (Invalid PC load)\r\n");
+    }
+    if (SCB->CFSR & (1 << 19)) {
+        printf("Cause: NOCP (No Coprocessor - Check FPU settings)\r\n");
+    }
+
+    while (1);
+}
+
+void HardFault_Handler_C(uint32_t *stacked_regs)
+{
+    uint32_t r0  = stacked_regs[0];
+    uint32_t r1  = stacked_regs[1];
+    uint32_t r2  = stacked_regs[2];
+    uint32_t r3  = stacked_regs[3];
+    uint32_t r12 = stacked_regs[4];
+    uint32_t lr  = stacked_regs[5];
+    uint32_t pc  = stacked_regs[6];
+    uint32_t xpsr = stacked_regs[7];
+
+    printf("\r\n--- HardFault Handler ---\r\n");
+    printf("HFSR: 0x%08x\r\n", (unsigned int)SCB->HFSR);
+    printf("CFSR: 0x%08x\r\n", (unsigned int)SCB->CFSR);
+    printf("Stacked R0:  0x%08x\r\n", (unsigned int)r0);
+    printf("Stacked R1:  0x%08x\r\n", (unsigned int)r1);
+    printf("Stacked R2:  0x%08x\r\n", (unsigned int)r2);
+    printf("Stacked R3:  0x%08x\r\n", (unsigned int)r3);
+    printf("Stacked R12: 0x%08x\r\n", (unsigned int)r12);
+    printf("Stacked LR:  0x%08x\r\n", (unsigned int)lr);
+    printf("Stacked PC:  0x%08x\r\n", (unsigned int)pc);
+    printf("Stacked PSR: 0x%08x\r\n", (unsigned int)xpsr);
+
+    if (SCB->HFSR & (1 << 31)) {
+        printf("Cause: FORCED (Forced Hard Fault)\r\n");
+    }
+    if (SCB->HFSR & (1 << 30)) {
+        printf("Cause: VECTTBL (Vector Table Read Fault)\r\n");
+    }
+
+    while (1);
+}
 /* USER CODE END EV */
 
 /******************************************************************************/
@@ -85,16 +154,17 @@ void NMI_Handler(void)
 /**
   * @brief This function handles Hard fault interrupt.
   */
-void HardFault_Handler(void)
+__attribute__((naked)) void HardFault_Handler(void)
 {
   /* USER CODE BEGIN HardFault_IRQn 0 */
-  printf("HardFault Handler\r\n");
+  __asm volatile (
+    "tst lr, #4\n"
+    "ite eq\n"
+    "mrseq r0, msp\n"
+    "mrsne r0, psp\n"
+    "b HardFault_Handler_C\n"
+  );
   /* USER CODE END HardFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
-    /* USER CODE END W1_HardFault_IRQn 0 */
-  }
 }
 
 /**
@@ -103,7 +173,7 @@ void HardFault_Handler(void)
 void MemManage_Handler(void)
 {
   /* USER CODE BEGIN MemoryManagement_IRQn 0 */
-  printf("MemManage Handler\r\n");
+  printf("MemManage Handler! CFSR: 0x%08lx, MMFAR: 0x%08lx\r\n", SCB->CFSR, SCB->MMFAR);
   /* USER CODE END MemoryManagement_IRQn 0 */
   while (1)
   {
@@ -118,7 +188,7 @@ void MemManage_Handler(void)
 void BusFault_Handler(void)
 {
   /* USER CODE BEGIN BusFault_IRQn 0 */
-  printf("BusFault Handler\r\n");
+  printf("BusFault Handler! CFSR: 0x%08lx, BFAR: 0x%08lx\r\n", SCB->CFSR, SCB->BFAR);
   /* USER CODE END BusFault_IRQn 0 */
   while (1)
   {
@@ -130,16 +200,15 @@ void BusFault_Handler(void)
 /**
   * @brief This function handles Undefined instruction or illegal state.
   */
-void UsageFault_Handler(void)
+__attribute__((naked)) void UsageFault_Handler(void)
 {
-  /* USER CODE BEGIN UsageFault_IRQn 0 */
-  printf("UsageFault Handler\r\n");
-  /* USER CODE END UsageFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
-    /* USER CODE END W1_UsageFault_IRQn 0 */
-  }
+  __asm volatile (
+    "tst lr, #4\n"
+    "ite eq\n"
+    "mrseq r0, msp\n"
+    "mrsne r0, psp\n"
+    "b UsageFault_Handler_C\n"
+  );
 }
 
 /**
