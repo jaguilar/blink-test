6d9dbf90d9620e29b73b2c728acac5b862b012ae
James Aguilar <aguilar.james@gmail.com>
2026-04-06 09:46:38 -0600

    timer_sync.py fixes

--- sim/timer_sync.py
+++ sim/timer_sync.py
@@ -1,16 +1,33 @@
 # Renode script to simulate STM32 Timer TRGO/Slave sync for Renode 1.16.1
-from Antmicro.Renode.Logging import Logger
+from Antmicro.Renode.Logging import Logger, LogLevel
 from Antmicro.Renode.Time import TimeInterval
 
 class TimerSync:
-    def __init__(self, machine):
+    def __init__(self, machine, monitor):
         self.machine = machine
-        self.timers = {
-            "TIM1": machine.GetLocalObject("timer1"),
-            "TIM8": machine.GetLocalObject("timer8"),
-            "TIM20": machine.GetLocalObject("timer20"),
-        }
+        self.monitor = monitor
+        self.sb = machine.SystemBus
         
+        # Use full names found via GetAllNames
+        all_names = list(machine.GetAllNames())
+        
+        def find_timer(repl_name):
+            for n in all_names:
+                if n == repl_name or n == "sysbus." + repl_name or n.endswith("." + repl_name):
+                    try:
+                        return machine[n], n
+                    except:
+                        pass
+            return None, None
+
+        self.timer_objs = {}
+        self.timer_names = {}
+        
+        for name, repl_name in [("TIM1", "timers1"), ("TIM8", "timers8"), ("TIM20", "timers20")]:
+            obj, full_name = find_timer(repl_name)
+            self.timer_objs[name] = obj
+            self.timer_names[name] = full_name
+
         # Follower -> { ITR_Index: Leader_Name }
         self.itr_map = {
             "TIM1": { 5: "TIM8", 9: "TIM20" },
@@ -25,47 +42,64 @@ class TimerSync:
         self.CCR4 = 0x40
         self.CNT = 0x24
 
-        for name, timer in self.timers.items():
-            if timer:
-                # AddPostWriteHook on the CR1 register (offset 0)
-                timer.AddPostWriteHook(self.CR1, self.create_on_cr1_write(name))
-                self.machine.Log(2, "TimerSync: Attached hook to {}".format(name))
+        # Register in global context for hooks
+        global TimerSyncLogic
+        TimerSyncLogic = self
 
-    def create_on_cr1_write(self, name):
-        # Create a closure to capture the timer name properly
-        return lambda addr, val: self.on_cr1_write(name, val)
+        for name, timer in self.timer_objs.items():
+            if timer is not None:
+                full_name = self.timer_names[name]
+                # Use Monitor command via monitor.Parse to avoid IronPython generic issues
+                hook_code = "TimerSyncLogic.on_write('{}', offset, value)".format(name)
+                
+                mon_name = full_name
+                if mon_name.startswith("sysbus."):
+                    mon_name = mon_name[len("sysbus."):]
+                
+                cmd = "sysbus SetHookBeforePeripheralWrite {} \"{}\"".format(mon_name, hook_code)
+                try:
+                    self.monitor.Parse(cmd)
+                    self.machine.Log(LogLevel.Info, "TimerSync: Attached hook to {} ({})".format(name, mon_name))
+                except Exception as e:
+                    self.machine.Log(LogLevel.Error, "TimerSync: Error attaching hook to {}: {}".format(name, str(e)))
+            else:
+                self.machine.Log(LogLevel.Warning, "TimerSync: Timer {} NOT FOUND in machine".format(name))
 
-    def on_cr1_write(self, name, val):
-        cen = (val & 1) != 0
-        if cen:
-            timer = self.timers[name]
-            cr2 = timer.ReadDoubleWord(self.CR2)
-            mms = (cr2 >> 4) & 0x7
-            if mms == 7: # OC4REF mode for TRGO
-                ccr4 = timer.ReadDoubleWord(self.CCR4)
-                cnt = timer.ReadDoubleWord(self.CNT)
-                if ccr4 > cnt:
-                    # Timer in .repl is 100MHz = 10ns per tick
-                    delay_ticks = ccr4 - cnt
-                    delay_ns = delay_ticks * 10
-                    self.machine.Log(2, "TimerSync: Leader {} starting, scheduling trigger in {}ns".format(name, delay_ns))
-                    # Schedule an action in virtual time
-                    self.machine.StandardControls.ScheduleAction(TimeInterval.FromNanoseconds(delay_ns), lambda: self.fire_trigger(name))
+    def on_write(self, name, offset, value):
+        if offset == self.CR1:
+            cen = (value & 1) != 0
+            if cen:
+                timer = self.timer_objs[name]
+                cr2 = timer.ReadDoubleWord(self.CR2)
+                mms = (cr2 >> 4) & 0x7
+                if mms == 7: # OC4REF mode for TRGO
+                    ccr4 = timer.ReadDoubleWord(self.CCR4)
+                    cnt = timer.ReadDoubleWord(self.CNT)
+                    if ccr4 > cnt:
+                        delay_ticks = ccr4 - cnt
+                        delay_ns = delay_ticks * 10
+                        self.machine.Log(LogLevel.Info, "TimerSync: Leader {} starting, scheduling trigger in {}ns".format(name, delay_ns))
+                        self.machine.ScheduleAction(TimeInterval.FromNanoseconds(delay_ns), lambda: self.fire_trigger(name))
 
     def fire_trigger(self, leader_name):
-        self.machine.Log(2, "TimerSync: Firing trigger from leader {}".format(leader_name))
-        for follower_name, follower_timer in self.timers.items():
-            if follower_name == leader_name: continue
+        self.machine.Log(LogLevel.Info, "TimerSync: Firing trigger from leader {}".format(leader_name))
+        for follower_name, follower_timer in self.timer_objs.items():
+            if follower_name == leader_name or follower_timer is None:
+                continue
             smcr = follower_timer.ReadDoubleWord(self.SMCR)
             sms = (smcr & 0x7) | ((smcr >> 13) & 0x8)
             if sms == 6: # Trigger Mode
                 ts = ((smcr >> 4) & 0x7) | ((smcr >> 17) & 0x18)
                 mapping = self.itr_map.get(follower_name, {})
                 if mapping.get(ts) == leader_name:
-                    self.machine.Log(2, "TimerSync: Triggering follower {}".format(follower_name))
+                    self.machine.Log(LogLevel.Info, "TimerSync: Triggering follower {}".format(follower_name))
                     cr1 = follower_timer.ReadDoubleWord(self.CR1)
                     follower_timer.WriteDoubleWord(self.CR1, cr1 | 1)
 
 # Instance creation
-# Note: Renode exposes 'machine' in this context
-TimerSync(machine)
+try:
+    ts_obj = TimerSync(machine, monitor)
+except NameError:
+    ts_obj = TimerSync(self.Machine, self)
+except Exception as e:
+    print("TimerSync initialization failed: " + str(e))
